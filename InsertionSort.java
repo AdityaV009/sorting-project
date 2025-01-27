9d4cc132cc656726c6f44917f84f019f1a3f866b
Aditya V <132118864+AdityaV009@users.noreply.github.com>
2025-01-27 07:00:53 +0000

    added  slection sort code and fixed inseriton sort code

--- InsertionSort.java
+++ InsertionSort.java
@@ -2,5 +2,15 @@ public class InsertionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Insertion Sort!!!");
+        int temp; 
+        for(int i = 1; i < input.length; i++) {
+            int j = i;
+            while(j > 0 && input[j - 1] > input[j]) {
+                temp = input[j];
+                input[j] = input[j - 1];
+                input[j - 1] = temp;
+                j--; 
+            }
+        }
     }
 }
